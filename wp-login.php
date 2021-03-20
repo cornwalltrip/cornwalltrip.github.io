b044bc7c8391a422759b6e073402207593181c54
badgerbadger1234@hotmail.com <badgerbadger1234@hotmail.com>
2021-03-20 09:58:07 +0000

    https instead of http

--- wp-login.php
+++ wp-login.php
@@ -7,17 +7,17 @@
 		<script>
 			/* <![CDATA[ */
 			var rcewpp = {
-				"ajax_url":"http://cornwalltrip.github.io/wp-admin/admin-ajax.php",
+				"ajax_url":"https://cornwalltrip.github.io/wp-admin/admin-ajax.php",
 				"nonce": "79b605c3ab",
-				"home_url": "http://cornwalltrip.github.io"
+				"home_url": "https://cornwalltrip.github.io"
 			};
 			/* ]]\> */
 		</script>
-		<link rel="stylesheet" id="dashicons-css" href="http://cornwalltrip.github.io/wp-includes/css/dashicons.min.css?ver=5.6" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="http://cornwalltrip.github.io/wp-includes/css/buttons.min.css?ver=5.6" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="http://cornwalltrip.github.io/wp-admin/css/forms.min.css?ver=5.6" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="http://cornwalltrip.github.io/wp-admin/css/l10n.min.css?ver=5.6" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="http://cornwalltrip.github.io/wp-admin/css/login.min.css?ver=5.6" type="text/css" media="all">
+		<link rel="stylesheet" id="dashicons-css" href="https://cornwalltrip.github.io/wp-includes/css/dashicons.min.css?ver=5.6" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://cornwalltrip.github.io/wp-includes/css/buttons.min.css?ver=5.6" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://cornwalltrip.github.io/wp-admin/css/forms.min.css?ver=5.6" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://cornwalltrip.github.io/wp-admin/css/l10n.min.css?ver=5.6" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://cornwalltrip.github.io/wp-admin/css/login.min.css?ver=5.6" type="text/css" media="all">
 	<meta name="robots" content="noindex,noarchive">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
@@ -30,7 +30,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="http://cornwalltrip.github.io/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://cornwalltrip.github.io/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -48,34 +48,34 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
-									<input type="hidden" name="redirect_to" value="http://cornwalltrip.github.io/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://cornwalltrip.github.io/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="http://cornwalltrip.github.io/wp-login.php?action=lostpassword">Lost your password?</a>
+								<a href="https://cornwalltrip.github.io/wp-login.php?action=lostpassword">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="http://cornwalltrip.github.io/">
+				<p id="backtoblog"><a href="https://cornwalltrip.github.io/">
 		&larr; Go to Cornwall Trip		</a></p>
 			</div>
-	<script type="text/javascript" src="http://cornwalltrip.github.io/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
-<script type="text/javascript" src="http://cornwalltrip.github.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+	<script type="text/javascript" src="https://cornwalltrip.github.io/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://cornwalltrip.github.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script type="text/javascript" id="zxcvbn-async-js-extra">
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"http:\/\/cornwalltrip.github.io\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/cornwalltrip.github.io\/wp-includes\/js\/zxcvbn.min.js"};
 /* ]]> */
 </script>
-<script type="text/javascript" src="http://cornwalltrip.github.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="http://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://cornwalltrip.github.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
 <script type="text/javascript" id="wp-polyfill-js-after">
-( 'fetch' in window ) || document.write( '<script src="http://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="http://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="http://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="http://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="http://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="http://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );
+( 'fetch' in window ) || document.write( '<script src="https://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://cornwalltrip.github.io/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );
 </script>
-<script type="text/javascript" src="http://cornwalltrip.github.io/wp-includes/js/dist/i18n.min.js?ver=326fe7fbfdb407b6edbcfba7e17f3909" id="wp-i18n-js"></script>
+<script type="text/javascript" src="https://cornwalltrip.github.io/wp-includes/js/dist/i18n.min.js?ver=326fe7fbfdb407b6edbcfba7e17f3909" id="wp-i18n-js"></script>
 <script type="text/javascript" id="password-strength-meter-js-extra">
 /* <![CDATA[ */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
@@ -88,14 +88,14 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="http://cornwalltrip.github.io/wp-admin/js/password-strength-meter.min.js?ver=5.6" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="http://cornwalltrip.github.io/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
+<script type="text/javascript" src="https://cornwalltrip.github.io/wp-admin/js/password-strength-meter.min.js?ver=5.6" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://cornwalltrip.github.io/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">
 /* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 /* ]]> */
 </script>
-<script type="text/javascript" src="http://cornwalltrip.github.io/wp-includes/js/wp-util.min.js?ver=5.6" id="wp-util-js"></script>
+<script type="text/javascript" src="https://cornwalltrip.github.io/wp-includes/js/wp-util.min.js?ver=5.6" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
@@ -103,7 +103,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="http://cornwalltrip.github.io/wp-admin/js/user-profile.min.js?ver=5.6" id="user-profile-js"></script>
+<script type="text/javascript" src="https://cornwalltrip.github.io/wp-admin/js/user-profile.min.js?ver=5.6" id="user-profile-js"></script>
 	<div class="clear"></div>
 	<div id="turnkey-credit"> <div style="clear: both;"> <a href="https://www.turnkeylinux.org/wordpress">WordPress Appliance</a> - Powered by <a href="https://www.turnkeylinux.org/">TurnKey Linux</a> </div> </div>
 </body>
